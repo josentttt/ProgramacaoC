14242434e38e23cd45d0b5eeae19f49420ddd22d
josentttt <almeidant06@gmail.com>
2026-05-11 18:47:28 +0000

    Aulas de programação C - Nível Novato, Aventureiro e Mestre

---
+++ NivelMestre/Video1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long long int numeroGrande = 4000000000;
+    int numero = 4000000000;
+    
+    printf("Valor de numeroGrandePositivo: %lu\n", numeroGrandePositivo);
+    printf("Valor de numeroPositivo: %u\n", numeroPositivo);
+    printf("Valor de numeroGrande: %lld\n", numeroGrande);
+    printf("Valor de numero: %d\n", numero);
+    
+    return 0;
+}
--- olamundo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    printf("Olá, Mundo!\n");
-
-    return 0;
-
-}
